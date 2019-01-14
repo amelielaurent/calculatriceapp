39192e6564c3d36bf9cd5d8baa9705b98bc7ee92
Robin Simonklein <rsimonklein@gmail.com>
2019-01-14 13:56:27 +0100

    Conversion de HistoriqueActivity en HistoriqueFragment

---
+++ app/src/main/java/com/calculatricegobelins/user/testcalculatrice/fragments/HistoriqueFragment.java
@@ -0,0 +1,20 @@
+package com.calculatricegobelins.user.testcalculatrice.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.calculatricegobelins.user.testcalculatrice.R;
+
+public class HistoriqueFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.historique_fragment, container, false);
+
+        return view;
+    }
+}
