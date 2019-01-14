ee3ca3bfb812d2c03a4d7675673b0e4c6ee98c9f
Robin Simonklein <rsimonklein@gmail.com>
2019-01-14 14:21:45 +0100

    Ajout d'un bouton pour accéder à l'historique

--- app/src/main/java/com/calculatricegobelins/user/testcalculatrice/fragments/MainFragment.java
+++ app/src/main/java/com/calculatricegobelins/user/testcalculatrice/fragments/MainFragment.java
@@ -4,23 +4,45 @@ import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import com.calculatricegobelins.user.testcalculatrice.R;
 import com.calculatricegobelins.user.testcalculatrice.models.OperationType;
 
-public class MainFragment extends Fragment {
+public class MainFragment extends Fragment{
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.main_fragment, container, false);
 
+        ImageView btn_historique = view.findViewById(R.id.bt_historique);
+
+        btn_historique.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showHistorique();
+            }
+        });
 
         return view;
+    }
+
+    public void showHistorique(){
+        HistoriqueFragment fragment = new HistoriqueFragment();
+
+        FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+
+        transaction.replace(R.id.fragment_container, fragment);
+
+        transaction.addToBackStack(null);
+
+        transaction.commit();
     }
 
 
